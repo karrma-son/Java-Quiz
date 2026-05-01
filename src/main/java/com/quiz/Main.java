6fb23392330dde13bebfd9623ca2c4c258cb2014
Mason Karr <karr.mason@gmail.com>
2026-05-01 14:49:56 -0500

    Scanner input and do-while loop

--- src/main/java/com/quiz/Main.java
+++ src/main/java/com/quiz/Main.java
@@ -1,15 +1,47 @@
 package com.quiz;
 
+import com.quiz.inputs.ScannerInput;
 import com.quiz.inputs.model.User;
 
 public class Main {
     static void main() {
 
-        User myUser = new User();
-        User Marshawn = new User();
+        ScannerInput input = new ScannerInput();
 
-        Marshawn.setFirstName("Mason");
-        System.out.println(Marshawn.getFirstName());
+        char answer;
+        boolean isAlphabet;
+        do {
+            input.showQuestion();
+            answer = input.getChar();
+            System.out.println("input " + answer);
+            isAlphabet = input.isAlphabet(answer);
+            if (!isAlphabet) {
+                System.err.println("Please enter a character A-Z");
+            }
+        } while (!isAlphabet);
+
+
+
+        if ( answer == 'A' ) {
+            System.out.println("correct");
+        } else {
+            System.out.println("incorrect");
+        }
+
+//        System.out.println("Please enter your first name");
+//        String firstName = input.getString();
+//
+//        System.out.println("Please enter your last name");
+//        String lastName = input.getString();
+//
+//        System.out.println(firstName + " " + lastName);
+
+
+//        User myUser = new User();
+//        User Marshawn = new User();
+//
+//        Marshawn.setFirstName("Mason");
+//        System.out.println(Marshawn.getFirstName());
 
 
 //        System.out.println(myUser.FirstName);
--- src/main/java/com/quiz/inputs/ScannerInput.java
+++ src/main/java/com/quiz/inputs/ScannerInput.java
@@ -4,10 +4,47 @@ import java.util.Scanner;
 
 public class ScannerInput {
 //    private Scanner scanner = new Scanner(System.in);
-    private final Scanner scanner;
+    private final Scanner myScanner;
 
-    public ScannerInput() {
-        scanner = new Scanner(System.in);
+    public void showQuestion() {
+        System.out.println("Who was the first super saiayan?");
+        System.out.println("A. Goku");
+        System.out.println("B. Vegeta");
+        System.out.println("C. Chichi");
+        System.out.print("\nEnter your choice: ");
     }
 
+//    public boolean isAlphabet(char character){
+//        String string = character + "";
+//        char uppercaseCharacter = string.toUpperCase().charAt(0);
+//        return uppercaseCharacter >= 'A' && uppercaseCharacter <= 'Z';
+//    }
+
+    public boolean isAlphabet(char character) {
+         for( char i = 'a'; i <= 'z'; i++){
+             String letterString = character + "";
+             char upperCaseLetter = letterString.toUpperCase().charAt(0);
+             if (i == upperCaseLetter) {
+
+                return true;
+            }
+         }
+
+
+
+         return false;
+    }
+
+    public ScannerInput() {
+        myScanner = new Scanner(System.in);
+    }
+    public String getString() {
+        return myScanner.nextLine();
+    }
+
+    public char getChar() {
+       return myScanner.nextLine().toUpperCase().charAt(0);
+
+
+    }
 }
